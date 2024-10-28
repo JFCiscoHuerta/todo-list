669acba7efbf0ed55e59db387db07fdd3b537a96
JFCiscoHuerta <151865620+JFCiscoHuerta@users.noreply.github.com>
2024-10-28 00:55:28 -0600

    chore: add ToDoApplication documentation

--- src/main/java/com/gklyphon/ToDo/ToDoApplication.java
+++ src/main/java/com/gklyphon/ToDo/ToDoApplication.java
@@ -3,6 +3,12 @@ package com.gklyphon.ToDo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *
+ * @author JFCiscoHuerta
+ * @version 1.0
+ * @since 28-Oct-2024
+ */
 @SpringBootApplication
 public class ToDoApplication {
 
