8aa2048016511b274d20284019f7bd47881b66e4
JFCiscoHuerta <151865620+JFCiscoHuerta@users.noreply.github.com>
2024-10-28 00:48:22 -0600

    chore: add ITaskRepository documentation

--- src/main/java/com/gklyphon/ToDo/repository/ITaskRepository.java
+++ src/main/java/com/gklyphon/ToDo/repository/ITaskRepository.java
@@ -3,5 +3,20 @@ package com.gklyphon.ToDo.repository;
 import com.gklyphon.ToDo.model.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Repository interface for managing {@link Task} entities.
+ * <p>
+ * This interface extends {@link JpaRepository}, providing CRUD operations
+ * and other database interactions for the {@code Task} entity.
+ * </p>
+ * <p>
+ * No implementation is required as Spring Data JPA automatically provides
+ * the implementation at runtime.
+ * </p>
+ *
+ * @author JFCiscoHuerta
+ * @version 1.0
+ * @since 28-Oct-2024
+ */
 public interface ITaskRepository extends JpaRepository<Task, Long> {
 }
