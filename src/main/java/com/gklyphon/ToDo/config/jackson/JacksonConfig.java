35486ac26ea50a5ab3e5a591d460c3b6566b396b
JFCiscoHuerta <151865620+JFCiscoHuerta@users.noreply.github.com>
2024-10-28 00:44:36 -0600

    chore: add JacksonConfig documentation

--- src/main/java/com/gklyphon/ToDo/config/jackson/JacksonConfig.java
+++ src/main/java/com/gklyphon/ToDo/config/jackson/JacksonConfig.java
@@ -5,9 +5,27 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Configuration class for customizing the Jackson ObjectMapper.
+ * <p>
+ * This class is responsible for creating and configuring a
+ * {@link ObjectMapper} bean with the necessary modules for
+ * serializing and deserializing Java 8 time types.
+ * </p>
+ *
+ * @author JFCiscoHuerta
+ * @version 1.0
+ * @since 28-Oct-2024
+ */
 @Configuration
 public class JacksonConfig {
 
+    /**
+     * Creates a new instance of {@link ObjectMapper} and registers
+     * the {@link JavaTimeModule} to support Java 8 date and time types.
+     *
+     * @return a configured {@link ObjectMapper} instance
+     */
     @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
